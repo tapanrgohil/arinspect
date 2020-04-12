2be7383414f88abe21c8b1472dbd8d46707528fb
tapan gohil <tapanmyid@gmail.com>
2020-04-12 13:32:45 +0530

    Rearrange presentation package and renamed MainActivity

--- app/src/main/java/com/tapan/facts/presentation/core/BaseActivity.kt
+++ app/src/main/java/com/tapan/facts/presentation/core/BaseActivity.kt
@@ -1,4 +1,4 @@
-package com.tapan.facts.presentation
+package com.tapan.facts.presentation.core
 
 import android.os.Build
 import android.os.Bundle
--- app/src/main/java/com/tapan/facts/presentation/fact/FactActivity.kt
+++ app/src/main/java/com/tapan/facts/presentation/fact/FactActivity.kt
@@ -1,12 +1,13 @@
-package com.tapan.facts
+package com.tapan.facts.presentation.fact
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tapan.facts.R
 
-class MainActivity : AppCompatActivity() {
+class FactActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_fact)
     }
 }
